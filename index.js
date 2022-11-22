39534ad4b178bb154180165b50a0be373e87cf5d
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2022-11-22 14:59:20 +0700

    feat: create api post img

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use("/docs", swaggerUI.serve, swaggerUI.setup(docs));
 app.use(compression(6))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use('/public/uploads/images', express.static(__dirname + '/public/uploads/images'));
 
 
 connectDB(process.env.MONGO_URL || "mongodb://localhost:27017/projectDB");
@@ -57,13 +58,48 @@ app.post('/uploadStockCSV', uploads.single('csvFile'), (req, res) => {
             date: new Date(item?.date),
           });
           try {
-              await newStock.save()
+            await newStock.save()
           } catch (error) {
-              console.log(error);
-          }}
-        }, (err) => {  return res.status(400).json({ status: 0, message: err }); })
-        return res.status(200).json({ status: 1, message: "inserted" });
+            console.log(error);
+          }
+        }
+      }, (err) => { return res.status(400).json({ status: 0, message: err }); })
+      return res.status(200).json({ status: 1, message: "inserted" });
     })
+})
+
+const getExtension = file => {
+  if (file.mimetype == "image/jpeg")
+    ext = ".jpeg"
+  else
+    ext = ".png"
+  return ext;
+}
+
+var storageImg = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + getExtension(file))
+  }
+})
+var uploadImg = multer({ storage: storageImg })
+
+app.post('/uploadImg', uploadImg.single('img'), (req, res, next) => {
+  if (req.file && req.file.mimetype != 'image/jpeg' && req.file.mimetype != 'image/png')
+    return res.json({
+      status: 0,
+      message: "Please Choose JPG or PNG images"
+    })
+  if (req.file) {
+    let image =  "http://127.0.0.1:3000/public/uploads/images/" + req.file.filename
+    res.json({
+      status: 1,
+      message: "Successfully saved",
+      path: image
+    })
+  }
 })
 
 app.listen(PORT, function () {
